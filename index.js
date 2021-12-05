7495a155373479f801ee613217230f3155c4187c
Shashank Sundriyal <shashanksundriyal2000@gmail.com>
2021-12-05 18:17:22 +0530

    Setup Completed

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const sassMiddleware = require('node-sass');
 const port = 8000;
 const app = express();
+
+sassMiddleware.render({
+    file: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+});
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+app.use(expressLayouts);
+app.use(express.static('./assets'));
+
+app.use('/', require('./routes/index'));
 app.listen(port, function(err){
     if(err){
         console.log(`Error in starting the servor: ${err}`);
